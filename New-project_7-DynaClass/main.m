7e52f15ea1ac42b8d46e3af9ed58afdb8c8410fa
lidongjie <dongjie@weico.com>
2018-04-08 09:34:01 +0800

    数据类型：objc_method

--- New-project_7-DynaClass/main.m
+++ New-project_7-DynaClass/main.m
@@ -26,7 +26,18 @@ int main(int argc, char * argv[]) {
         Method description = class_getInstanceMethod([NSObject class],
                                                      @selector(description));
         const char *types = method_getTypeEncoding(description);
+        
+        /*
+        // 运行时系统库中的方法数据类型（objc_method）
+        struct objc_method {
+            SEL method_name;     // method_name 是一个类型为SEL的变量，它描述了方法的名称；
+            char *method_types;  // 变量 method_types 描述了方法参数的数据类型；
+            IMP method_imp;      // method_imp 是一个类型为IMP的变量，它在方法被调用时提供方法的地址；
+        };
+        typedef objc_method Method;
+        */
         class_addMethod(dynaClass, @selector(greeting), (IMP)greeting, types);
+        
         
         // 注册这个类
         objc_registerClassPair(dynaClass);
@@ -37,6 +48,7 @@ int main(int argc, char * argv[]) {
         
         // 注意：在目前版本的Xcode中，需要将 Apple LLVM X.X（版本号） - Preprocessing 中的 Enable Strict Checking of objc_msgSend Calls 设置为No，不然编译会失败。
         NSLog(@"%@",objc_msgSend(dynaObj, NSSelectorFromString(@"greeting")));
+        
         
         
         
